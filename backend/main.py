0e5a32b830a4f211b34e6e119d2fe46cb9c8f798
프랑스어 한스푼 <tonbonami@gmail.com>
2026-04-04 05:29:54 +0000

    Fix CORS allow_origins duplicate bug

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_origins=["https://orompdfunlock.vercel.app", "http://localhost:3000"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
